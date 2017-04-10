043d3f5f14706f30fe3a2281d8167fb5355fdb4c
akash_95 <akashdeepmandal1995@gmail.com>
2017-04-10 13:58:06 +0530

    transfer status not yet

--- conman.py
+++ conman.py
@@ -118,7 +118,7 @@ def server():
 def format_filename(s):
     valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
     filename = ''.join(c for c in s if c in valid_chars)
-    filename = filename.replace(' ','_') # I don't like spaces in filenames.
+    filename = filename.replace(' ','_')
     return filename
     
 def file_access():
